40668cfc57c7bcaa0201b9696946e4d8cd8d4e98
Madman10K <stanv.contact176@gmail.com>
2023-08-09 21:25:16 +0300

    Test with the UVKBuildTool

---
+++ UBTCustomFunctions/UBTCustomFunctions.cpp
@@ -0,0 +1,7 @@
+#ifdef UBT_TARGET_WEB
+#include "UBTCustomFunctions.hpp"
+
+void UBT::funcExportMain(UTTE::Generator& generator) noexcept
+{
+}
+#endif
---
+++ UBTCustomFunctions/UBTCustomFunctions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#ifdef UBT_TARGET_WEB
+#include "../Utils.hpp"
+
+namespace UBT
+{
+    UBT_PUBLIC_API void funcExportMain(UTTE::Generator& generator) noexcept;
+}
+#endif
