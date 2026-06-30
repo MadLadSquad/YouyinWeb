a6e6ebe64201d78746fa61a16226a024e8f0c8d8
Madman10K <stanv.contact176@gmail.com>
2026-06-30 04:20:02 +0300

    Fix errors on migration

--- ci-clean.sh
+++ ci-clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm *.html
+rm -rf *.html scripts/ styles/ sw.js manifest.json robots.txt sitemap.xml favicon*.png icon-*.png
 rm -rf Translations/ Components/ UBTCustomFunctions/ UVKBuildTool/ .github/
 mv build/* .
 rm -rf build
